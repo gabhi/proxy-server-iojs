03a378c5c8eafc9a846182a0e9f28520da39bbc0
Abhijit Gaikwad <AGaikwad@walmartlabs.com>
2015-04-21 21:53:11 -0700

    Echo server

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let http = require('http')
+let destinationUrl = '127.0.0.1:8000'
+
+
+//Echo server
 
 http.createServer((req, res) => {
-    console.log(`Request received at: ${req.url}`)
-    res.end('hello world\n')
+    console.log('Request received at: ${req.url}');
+    for (let header in req.headers) {
+        res.setHeader(header, req.headers[header])
+    }
+    req.pipe(res);
 }).listen(8000)
